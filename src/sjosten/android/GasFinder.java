7c63ef64f26614b7670e56dee30aaf5c7871f45c
Alexander Sjösten <sjosten.alexander@gmail.com>
2014-07-18 18:12:31 +0200

    code for having a simple map showing added

--- src/sjosten/android/GasFinder.java
+++ src/sjosten/android/GasFinder.java
@@ -6,7 +6,7 @@ import android.os.Bundle;
 public class GasFinder extends Activity {
     /** Called when the activity is first created. */
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
     }
