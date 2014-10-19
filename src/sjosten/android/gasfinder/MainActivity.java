f72a638a487dc6cc85f501c8775720e58ec863c5
Alexander Sjösten <sjosten.alexander@gmail.com>
2014-10-19 23:59:14 +0200

    about option in menu added and implemented
    About option in menu

--- src/sjosten/android/gasfinder/MainActivity.java
+++ src/sjosten/android/gasfinder/MainActivity.java
@@ -8,6 +8,8 @@ import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
@@ -37,23 +39,36 @@ public class MainActivity extends Activity {
 			.icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_launcher)));
 		}
 	}
-	
+
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
-	
+
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
 		switch(item.getItemId()) {
 		case R.id.action_download:
-			
+
 			return true;
 		case R.id.action_settings:
 			Intent intent = new Intent(this, SettingsActivity.class);
 			startActivity(intent);
 			return true;
+		case R.id.action_about:
+			AlertDialog alertDialog = new AlertDialog.Builder(this).create();
+			alertDialog.setTitle(R.string.about_title);
+			alertDialog.setMessage(getString(R.string.about_content));
+			alertDialog.setButton(AlertDialog.BUTTON_POSITIVE, getString(R.string.about_btn_close), new DialogInterface.OnClickListener() {
+				
+				@Override
+				public void onClick(DialogInterface dialog, int which) {
+					dialog.dismiss();	
+				}
+			});
+			
+			alertDialog.show();
 		default:
 			return super.onOptionsItemSelected(item);
 		}
